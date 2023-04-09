103896c46073b91e108cc4e5b3747d08afa2b39c
Luiz Felipe Zimbrão Gomes <felipe.zimbrao@agenciam3.com>
2023-04-09 14:49:25 -0300

    feat: routes implementation with react-router-dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,60 +1,24 @@
-import { FormEventHandler, useEffect, useState } from "react";
+import { BrowserRouter } from 'react-router-dom';
 
 import NavigateBar from "../NavigateBar/NavigateBar";
-import Field from "../Field/Field";
-import Form from "../Form/Form";
-import Submit from "../Submit/Submit";
-import AddMore from "../AddMore/AddMore";
-import RemoveItem from "../RemoveItem/RemoveItem";
 
-import readUploadFile from "../../utils/readUploadFile";
+import Routes from '../../Routes';
 
 import styles from "./App.module.css";
+import getCookie from '../../utils/getCookie';
 
 function App() {
-  const [textInput, setTextInput] = useState("");
-  const [fileInput, setFileInput] = useState({ value: "", files: "" });
-
-  const formSubmitFunction: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    return;
-  }
-
-  async function readUpload() {
-    if (fileInput.value) {
-      const res = await readUploadFile(fileInput.files[0]);
-
-      console.log("res", res)
-    }
-  }
-
-  useEffect(() => {
-    readUpload();
-  }, [fileInput])
-
+  const token = getCookie("token");
+  
   return (
-    <div className={styles["aplication--container"]}>
-      <NavigateBar />
-      <div className={styles["aplication--content"]}>
-        <Form formSubmitFunction={formSubmitFunction}>
-          <Field type="text" value={textInput} change={setTextInput} placeholder="Link Url" />
-          <Field type="text" value={textInput} change={setTextInput} placeholder="Link Text" />
-          <Field type="text" value={textInput} change={setTextInput} placeholder="Name" />
-          <Field type="text" value={textInput} change={setTextInput} placeholder="Name" />
-          <Field
-            type="file"
-            value={fileInput.value}
-            text={fileInput.value}
-            change={setFileInput}
-            placeholder="Escolha uma lista com os seus produtos"
-            accept=".xls,.xlsx,.csv"
-          />
-          <AddMore />
-          <RemoveItem />
-          <Submit text="SALVAR"/>
-        </Form>
+    <BrowserRouter>
+      <div className={styles["aplication--container"]}>
+        {token ? <NavigateBar /> : null}
+        <div className={styles["aplication--content"]}>
+          <Routes />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
